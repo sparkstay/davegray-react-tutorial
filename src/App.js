12fd73f268585b81f1276b81b842fd10eaa1f937
Taylin Sparks <taylinsparks7@gmail.com>
2024-03-06 10:44:06 -0800

    added header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './header';
 
 function App() {
   const handleNameChange = () => {
@@ -9,12 +9,7 @@ function App() {
   }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello {handleNameChange()}!
-        </p>
-      </header>
+      <Header />
     </div>
   );
 }
